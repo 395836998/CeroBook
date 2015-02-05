40c41459ac9c1da4123fedd1aa7ecd280e7408b6
zhangbook <395836998@qq.com>
2015-02-05 22:34:39 +0800

    to p25

---
+++ p25_ex_1_20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define TAB	8
+
+int main(){
+	int c, nb, pos;
+	nb = 0;
+	pos = 1;
+
+	while( (c=getchar()) != EOF){
+		if(c == '\t'){
+			nb = TAB - (pos-1)%TAB;
+			while(nb > 0){
+				putchar(' ');
+				pos++;
+				nb--;
+			}
+		}else if(c == '\n'){
+			putchar(c);
+			pos = 1;
+		}else {
+			putchar(c);
+			pos++;
+		}
+	}
+	return 0;
+}
